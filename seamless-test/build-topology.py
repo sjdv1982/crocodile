997d9a68349922d624070a0664b99f01bb867a13
Sjoerd de Vries <sjdv1982@gmail.com>
2021-05-08 17:27:38 +0200

    snapshot

---
+++ seamless-test/build-topology.py
@@ -0,0 +1,159 @@
+import numpy as np
+
+from seamless.highlevel import Context, Cell, Transformer, Module
+ctx = Context()
+
+ctx.modules = Context()
+ctx.modules.build_rotamers = Module()
+ctx.modules.build_rotamers.dependencies = ["rotamers"]
+ctx.modules.build_rotamers.mount("util/build-rotamers.py", "r")
+ctx.modules.rotamers = Module()
+ctx.modules.rotamers.mount("util/rotamers.py", "r")
+ctx.modules.parse_pdb = Module()
+ctx.modules.parse_pdb.mount("crocodile/parse_pdb.py", "r")
+
+ctx.rmsd = Cell("float").set(4)
+
+def gen_rotations(n):
+    import numpy as np
+    from scipy.spatial.transform import Rotation
+    np.random.seed(0)
+    return Rotation.random(n).as_matrix()
+ctx.gen_rotations = gen_rotations
+ctx.translate()
+ctx.gen_rotations.inp.example = {}
+ctx.gen_rotations.inp.example.n = 0
+ctx.gen_rotations.inp.schema.properties.n.minimum = 0
+ctx.translate()
+ctx.n_random_rotations = Cell("int").set(2)  #.set(300000)
+ctx.gen_rotations.n = ctx.n_random_rotations
+ctx.gen_rotations.result.example.set(np.zeros((5,3,3)))
+ctx.gen_rotations.result.schema["form"].shape = -1, 3, 3
+
+ctx.random_rotations = ctx.gen_rotations
+ctx.translate()
+
+ctx.pdb = Cell("text")
+def parse_pdb(pdb):
+    return parse_pdb_module.parse_pdb(pdb)
+
+ctx.parse_pdb = parse_pdb
+ctx.parse_pdb.pdb = ctx.pdb
+ctx.parse_pdb.parse_pdb_module = ctx.modules.parse_pdb
+ctx.struc = ctx.parse_pdb
+ctx.struc.celltype = "binary"
+ctx.translate()
+
+###
+ctx.pdb.set(open("1AVXB-unbound-bb.pdb").read())
+###
+ctx.compute()
+
+def get_structure_tensor(struc):
+    import numpy as np
+    coor = np.zeros((len(struc), 3))
+    coor[:, 0] = struc["x"]
+    coor[:, 1] = struc["y"]
+    coor[:, 2] = struc["z"]
+    return build_rotamers.get_structure_tensor(coor)
+
+ctx.get_structure_tensor = get_structure_tensor
+ctx.get_structure_tensor.struc = ctx.struc
+ctx.get_structure_tensor.build_rotamers = ctx.modules.build_rotamers
+ctx.get_structure_tensor.rotamers = ctx.modules.rotamers
+ctx.structure_tensor = ctx.get_structure_tensor
+
+ctx.scalevec = Cell()
+ctx.translate()
+ctx.scalevec.example.set(np.empty(3))
+ctx.scalevec.schema["form"].shape = (3,)
+ctx.scalevec = ctx.structure_tensor[1]
+ctx.compute()
+
+def do_pre_analysis(rsize, rmsd, scalevec, maxcostfrac):
+    import numpy as np
+    from .build_rotamers import estimate_nclust_curve, estimate_rmsd_dist, get_best_clustering_hierarchy
+    r1size, r2size, r3size = rsize
+  
+    result = {}
+    est_size = estimate_nclust_curve(rmsd, scalevec, r1size, r2size)
+  
+    result["nclust_curve"] = est_size
+    rmsd_dist = estimate_rmsd_dist(list(est_size.keys()), scalevec, r3size)
+    result["rmsd_dist"] = rmsd_dist
+  
+    hierarchy = get_best_clustering_hierarchy(est_size, rmsd_dist, rmsd, maxcostfrac)
+    result["hierarchy"] = hierarchy 
+
+    return result
+
+ctx.do_pre_analysis = do_pre_analysis
+tf = ctx.do_pre_analysis
+tf.rsize = 1000, 5000, 5000000
+tf.rmsd = ctx.rmsd
+tf.scalevec = ctx.scalevec
+tf.maxcostfrac = 0.3#
+tf.rotamers = ctx.modules.rotamers
+tf.build_rotamers = ctx.modules.build_rotamers
+ctx.pre_analysis = tf.result
+ctx.translate()
+
+ctx.hierarchy = Cell("binary")
+ctx.hierarchy = ctx.pre_analysis.hierarchy
+
+MAX_ROTAMERS = 1e8
+tf = ctx.build_rotamers = Transformer()
+tf.random_rotations = ctx.random_rotations
+tf.scalevec = ctx.scalevec
+tf.hierarchy = ctx.hierarchy
+tf.language = "cpp"
+ctx.compute()
+tf.inp.example.random_rotations = np.zeros((5,3,3))
+tf.inp.example.scalevec = np.zeros(3)
+tf.inp.example.hierarchy = np.zeros(10)
+tf.schema.required = ["random_rotations", "scalevec", "hierarchy"]
+form = tf.schema.properties.random_rotations["form"]
+form.shape = -1, 3, 3
+form.contiguous = True
+form = tf.schema.properties.scalevec["form"]
+form.shape = (3,)
+form.contiguous = True
+form = tf.schema.properties.hierarchy["form"]
+form.contiguous = True
+tf.result.example.set(np.zeros((5,3,3)))
+form = tf.result.schema["form"]
+form.shape = (0, MAX_ROTAMERS), 3, 3
+form.contiguous = True
+ctx.compute()
+
+c = ctx.build_rotamers_code = Cell("code")
+c.language = "cpp"
+c.mount("build-rotamers.cpp")
+tf.code = c
+ctx.translate()
+ctx.compute(0.5)
+if c.value is None:
+    header = tf.header.value
+'''    
+    if header is not None:
+        code = header.replace("int transform", 'extern "C" int transform') + "\n"
+        code += """
+#include <cstdio>
+
+extern "C" int transform(
+    const HierarchyStruct* hierarchy, const RandomRotationsStruct* random_rotations, 
+    const ScalevecStruct* scalevec, ResultStruct *result
+) {
+    for (int i = 0; i < 3; i++) {
+        printf("%.3f ", scalevec->data[i]);
+    }
+    printf("\\n\\n");
+    return 0;
+}        
+"""        
+        c.set(code)
+'''
+
+ctx.save_graph("graph/build-rotamer.seamless")
+ctx.save_zip("graph/build-rotamer.zip")
+ctx.save_vault("vault")
---
+++ test/test-irmsd-grid-rotamer.py
@@ -0,0 +1,119 @@
+import itertools
+import numpy as np
+from numpy import sqrt
+from numpy.linalg import svd, det
+from crocodile.parse_pdb import parse_pdb
+
+def get_coor(pdb):
+    coor = np.zeros((len(pdb), 3))
+    coor[:, 0] = pdb["x"]
+    coor[:, 1] = pdb["y"]
+    coor[:, 2] = pdb["z"]
+    return coor
+
+receptor = parse_pdb(open("1AVXA-unbound-heavy.pdb").read())
+ligand = parse_pdb(open("1AVXB-unbound-heavy.pdb").read())
+ligand_coor = get_coor(ligand)
+ligand_coor -= ligand_coor.sum(axis=0)/len(ligand_coor)
+refe_receptor = parse_pdb(open("1AVXA-bound-heavy.pdb").read())
+refe_ligand = parse_pdb(open("1AVXB-bound-heavy.pdb").read())
+
+matrices = np.load("../test-roc/rocs.npy")
+
+from crocodile.imask import imask
+imasks = imask([refe_receptor, refe_ligand], cutoff=10)
+
+def calc_covar(atoms1, atoms2):
+    assert atoms1.shape == atoms2.shape
+    return atoms1.T.dot(atoms2)
+
+if_r = get_coor(receptor[imasks[0]])
+refe_if_r, refe_if_l = get_coor(refe_receptor[imasks[0]]), get_coor(refe_ligand[imasks[1]])
+
+lenr, lenl = len(refe_if_r), len(refe_if_l)
+natoms = lenr + lenl    
+
+refe_com = (refe_if_r.sum(axis=0) + refe_if_l.sum(axis=0)) / natoms
+refe_if_r -= refe_com
+refe_if_l -= refe_com
+refe_residual = (refe_if_r*refe_if_r).sum() + (refe_if_l*refe_if_l).sum()
+
+refe_r_sum = refe_if_r.sum(axis=0)
+refe_l_sum = refe_if_l.sum(axis=0)
+
+f = sqrt(lenl/natoms * lenr/natoms)
+
+def calc_irmsds(offsets):
+    vec1 = -offsets * lenl/natoms
+    vec2 = offsets + vec1
+
+    residual1 = residual_r + (2 * vec1 * r_sum).sum(axis=1) + (lenr * vec1 * vec1).sum(axis=1)
+    residual2 = residual_l + (2 * vec2 * l_sum).sum(axis=1) + (lenl * vec2 * vec2).sum(axis=1)
+    residual = residual1 + residual2
+
+    covar1 = covar_r + vec1[:, :, None] * refe_r_sum[None, None, :]
+    covar2 = covar_l + vec2[:, :, None] * refe_l_sum[None, None, :]
+    covar = covar1 + covar2
+
+    v, s, wt = np.linalg.svd(covar)
+    reflect = np.linalg.det(v) * np.linalg.det(wt)
+    s[:,-1] *= reflect
+    ss = np.maximum( (residual + refe_residual) - 2 * s.sum(axis=1), 0)
+    irmsd = np.sqrt(ss / natoms)
+    return irmsd
+
+
+subd = list(itertools.product([-0.5, 0.5], repeat=3))
+subd = np.array(subd)
+
+def irmsd_voxel(offset, gridlevel, threshold):
+    voxelsize = lowest_voxelsize * 2**(gridlevel-1)
+    corner = voxelsize * sqrt(3) 
+    irmsd_margin = corner * f
+    offsets = voxelsize * subd + offset
+    irmsds = calc_irmsds(offsets)
+    if gridlevel == 1:
+        for n, irmsd, curr_offset in zip(range(8), irmsds, offsets):
+            if irmsd < threshold:
+                #print("ACCEPT LOWEST", offset, n)
+                accept_lowest.append(curr_offset)
+    else:
+        for n, irmsd, curr_offset in zip(range(8), irmsds, offsets):
+            if irmsd < threshold - irmsd_margin:
+                #print("ACCEPT", gridlevel, offset, n)
+                accept.append((gridlevel, curr_offset))
+            elif irmsd > threshold + irmsd_margin:
+                #print("REJECT", gridlevel, offset, n)
+                pass # reject
+            else:
+                irmsd_voxel(curr_offset, gridlevel-1, threshold)
+
+lowest_voxelsize = 1
+max_gridlevel = 8
+threshold=1
+
+print("Voxel size:", lowest_voxelsize, "Å")
+print("Box size", lowest_voxelsize * 2**max_gridlevel, "Å")
+print("iRMSD threshold", threshold, "Å")
+print("Voxels: {:e}".format(8**max_gridlevel))
+for mnr, m in enumerate(matrices):
+    lig_rot = ligand_coor.dot(m)
+    if_l = lig_rot[imasks[1]]
+    ini_com = (if_r.sum(axis=0) + if_l.sum(axis=0)) / natoms
+    if_r -= ini_com
+    if_l -= ini_com
+
+    r_sum = if_r.sum(axis=0)
+    l_sum = if_l.sum(axis=0)
+
+    covar_r = calc_covar(if_r, refe_if_r)
+    covar_l = calc_covar(if_l, refe_if_l)
+
+    residual_r = (if_r*if_r).sum()
+    residual_l = (if_l*if_l).sum()
+
+    accept = []
+    accept_lowest = []
+
+    irmsd_voxel(np.zeros(3), max_gridlevel, threshold)
+    print(mnr+1, "Nodes:", len(accept), "leaves:", len(accept_lowest), "total:", sum([8**(a[0]-1) for a in accept])+len(accept_lowest))
