dc1de97911c5ec9c890d324b6d695878dfbc79a7
Sjoerd de Vries <sjdv1982@gmail.com>
2023-06-23 18:36:40 +0200

    seamless build rotamer workflow: fix reflection bug; rebuild workflow for Seamless 0.11

--- seamless-test/build-rotamers-command.py
+++ seamless-test/build-rotamers-command.py
@@ -44,11 +44,8 @@ if ctx.build_rotamers.result.checksum is None:
     status(ctx)
     sys.exit(1)
 
-arr = ctx.build_rotamers.result.value.unsilk
-print(f"OUTPUT {max_rotations} random rotations => {len(arr)} clusters")
-if outfile is not None:
-    np.save(outfile, arr)
-
+clusters = ctx.build_rotamers.result.value.unsilk
+print(f"OUTPUT {max_rotations} random rotations => {len(clusters)} clusters")
 
 # check
 print("Checking output", file=sys.stderr)
@@ -67,19 +64,34 @@ rmsdref = np.sqrt(m.get_msd(verif, refe=None, scalevec=scalevec))
 struc = ctx.struc.value
 coor = np.stack((struc["x"], struc["y"], struc["z"]),axis=1)
 coor -= coor.mean(axis=0)
+coorx = coor.dot(tensor)
 verif0 = verif[:10]
-verif0_coors = np.einsum("ij,kjl->kil", coor, verif0) # broadcasted coor.dot(verif0[k])
-dref0 = verif0_coors - coor
+verif0_coors = np.einsum("ij,kjl->kil", coorx, verif0) # broadcasted coorx.dot(verif0[k])
+dref0 = verif0_coors - coorx
 rmsdref0 = np.sqrt((dref0*dref0).sum(axis=2).mean(axis=1))
-assert np.all(np.abs(rmsdref0 - rmsdref[:10]) < 0.02)
+assert np.all(np.abs(rmsdref0 - rmsdref[:10]) < 0.0001), (rmsdref0, rmsdref[:10])
+
+verifx = np.einsum("ij,kjl->kil", tensor, verif) #broadcasted tensor.dot(verif[k])
+verif0x = verifx[:10]
+verif0_xcoors = np.einsum("ij,kjl->kil", coor, verif0x)
+dref0 = verif0_xcoors - coor.dot(tensor)
+rmsdref0 = np.sqrt((dref0*dref0).sum(axis=2).mean(axis=1))
+assert np.all(np.abs(rmsdref0 - rmsdref[:10]) < 0.0001), (rmsdref0, rmsdref[:10])
+
 #/paranoid check
+
+verif = verif[:5000]  #
 
 not_in_cluster = 0
 for vnr, v in enumerate(verif):
     if not (vnr % 1000):
         print("{}/{}, not in cluster: {}".format(vnr+1, len(verif), not_in_cluster), file=sys.stderr)
-    vrmsd = np.sqrt(m.get_msd(arr, refe=v, scalevec=scalevec).min())
+    vrmsd = np.sqrt(m.get_msd(clusters, refe=v, scalevec=scalevec).min())
     if vrmsd > rmsd:
         not_in_cluster += 1
 print("OUTPUT Tested X random rotations, of which Y do not cluster within the {} threshold".format(rmsd))
 print(f"OUTPUT Y / X: {not_in_cluster} / {len(verif)}")
+
+clusters_backrotated = np.einsum("ij,kjl->kil", tensor, clusters) #broadcasted tensor.dot(verif[k])
+if outfile is not None:
+    np.save(outfile, clusters_backrotated)
